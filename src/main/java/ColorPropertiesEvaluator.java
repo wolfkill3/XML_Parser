5c754ae38a62d8b434e83f902cb874a44831032a
vadimm <vmelnikov@baltinfocom.ru>
2021-08-15 23:30:03 +0300

    Add logger and file writer

--- src/main/java/ColorPropertiesEvaluator.java
+++ src/main/java/ColorPropertiesEvaluator.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -15,16 +16,18 @@ import javax.xml.xpath.XPathExpression;
 import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
-import org.apache.log4j.Logger;
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-public class Main {
-    private static final Logger logger = Logger.getLogger(Main.class);
-    private static final String FILE_PATH = "src/main/resources/source_file.xml";
+public class ColorPropertiesEvaluator {
+    private static final Logger logger = LogManager.getLogger();
+    private static final String SOURCE_FILE_PATH = "src/main/resources/source_file.xml";
+    private static final String TARGET_FILE_PATH = "src/main/resources/target_file.xml";
     private static final String NODE_REGEX = ".//Neutral";
     private static final String PROPERTY_NAME = "Actor.Color";
     private static final boolean ADD_NEW_PROPERTY_FLAG = true; // if TRUE -> create Actor.Color property to Neutral node
@@ -37,15 +40,23 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        File file = new File(FILE_PATH);
+        File file = new File(SOURCE_FILE_PATH);
         try {
             Document document = buildDocument(file);
             NodeList nodes = getNodeList(document, NODE_REGEX);
             evaluateNodes(nodes, document);
-            System.out.println(nodeToString(document));
+            String docStrings = nodeToString(document);
+            //writeTargetFile(docStrings);
+            System.out.println(docStrings);
         } catch (SAXException | IOException | ParserConfigurationException | TransformerException | XPathExpressionException e) {
-            e.printStackTrace();
+            logger.error(e);
         }
+    }
+
+    private static void writeTargetFile(String docStrings) throws IOException {
+        OutputStream outputStream = new FileOutputStream(TARGET_FILE_PATH);
+        outputStream.write(docStrings.getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
     }
 
     private static Document buildDocument(File file) throws ParserConfigurationException, IOException, SAXException {
@@ -65,7 +76,7 @@ public class Main {
     private static String nodeToString(Document document) throws TransformerException {
         StringWriter writer = new StringWriter();
         Transformer transformer = TransformerFactory.newInstance().newTransformer();
-        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
         transformer.transform(new DOMSource(document), new StreamResult(writer));
         return (writer.toString());
     }
@@ -77,10 +88,12 @@ public class Main {
             Element element = (Element) getColorPropertyNode(neutralProperties);
             if (element != null) {
                 ATTRIBUTES.forEach(element::setAttribute);
+                logger.info("Evaluated color properties");
             } else if (ADD_NEW_PROPERTY_FLAG) {
                 Element colorProperty = document.createElement(PROPERTY_NAME);
                 ATTRIBUTES.forEach(colorProperty::setAttribute);
                 neutralNode.appendChild(colorProperty);
+                logger.info("Added color properties to node");
             }
         }
     }
